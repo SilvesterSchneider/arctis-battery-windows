a710adbbffc6fa1f1bdab3ac6eafc331c78fd699
Silvester Schneider <silvester_schneider@gmx.de>
2021-05-14 09:29:18 +0200

    finish

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace arctis_battery_windows
 {
@@ -6,7 +7,19 @@ namespace arctis_battery_windows
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("arctis-battery");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
         }
     }
 }
