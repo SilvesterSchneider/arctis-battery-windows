a87f61a34894bacef2348baee0e185cc96c84069
Silvester Schneider <silvester_schneider@gmx.de>
2021-05-14 09:24:32 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arctis_battery_windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
